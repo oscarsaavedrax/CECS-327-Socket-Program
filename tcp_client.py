3a708ffacf800e02a29dc520b9908d61b572d8bc
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2023-04-05 17:24:13 -0700

    Boilerplate for client

---
+++ tcp_client.py
@@ -0,0 +1,9 @@
+# Description: Echo client program that uses TCP to communicate with an Echo server. This program asks the user to enter the IP address, port number of the server, and a message for the server. The program sends the message to the server and listens for replies. The program displays an error message if the IP address or port number are incorrect.
+# Developer: Oscar Saavedra
+# Date: 04/07/2023
+
+def main():
+    print("client")
+    
+if __name__ == "__main__":
+    main()
