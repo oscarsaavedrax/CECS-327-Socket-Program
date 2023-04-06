66447806bb8ef127188e3be180a38f3d538a8c4e
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2023-04-05 22:32:02 -0700

    Boilerplate for server

---
+++ tcp_server.py
@@ -0,0 +1,9 @@
+# Description: Echo server program that uses TCP to communicate with an Echo client. This server receives a message from the client, changes the letters of the message to capital letters, then sends it back to the same client.
+# Developer: Oscar Saavedra
+# Date: 04/07/2023
+
+def main():
+    print()
+    
+if __name__ == "__main__":
+    main()
