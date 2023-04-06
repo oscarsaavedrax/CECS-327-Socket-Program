1a8c75804ae8eafc5aab4584d1d60ea0c21ae749
oscarsaavedrax <103982163+oscarsaavedrax@users.noreply.github.com>
2023-04-05 22:42:01 -0700

    Handle commandline arguments

--- tcp_server.py
+++ tcp_server.py
@@ -2,8 +2,21 @@
 # Developer: Oscar Saavedra
 # Date: 04/07/2023
 
-def main():
-    print()
+# Import argparse library to handle commandline arguments
+import argparse as agp
+
+def main(ip, port):
+    print(ip)
+    print(port)
     
 if __name__ == "__main__":
-    main()
+    # Create a parser to handle the commandline arguments
+    parser = agp.ArgumentParser(description="Echo server")
+    # Add argument information for IP address input
+    parser.add_argument("--ip", default="", help="IP address to bind the server (default: "")")
+    # Add argument information for port number input
+    parser.add_argument("--port", type=int, default=12345, help="Port to bind the server (default: 12345)")
+    # Parse the arguments
+    args = parser.parse_args()
+    # Start main and send in commandline arguments
+    main(args.ip, args.port)
